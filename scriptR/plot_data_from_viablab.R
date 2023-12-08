db75ef6675a4f0d1acfb9a939cad0660d0178aac
Etienne Delay <etienne.delay@cirad.fr>
2023-12-08 15:10:29 +0000

    read data `.dat`with R

--- scriptR/plot_data_from_viablab.R
+++ scriptR/plot_data_from_viablab.R
@@ -1,4 +1,15 @@
 ## read data from VIABLAB
+library(ggplot2)
+
 
 ## set working directory
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+## read data 
+### grid_data.dat    => les variables d'état et les valeurs min et max et discretiastion
+### viab-0-bound.dat => Tout les point du noyaux (chaque colone est un controle)
+### viab-0.dat       => le meme que bound, mais avec le contour
+data.df <- read.table("../OUTPUT/model2D--viab-0-bound.dat")
+
+ggplot(data = data.df)+
+  geom_point(aes(x = V1, y = V2))
