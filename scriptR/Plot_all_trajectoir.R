ded50a11a17e7a4b6fd5f6e690cace5a48f4b883
Etienne Delay <etienne.delay@cirad.fr>
2024-07-12 09:34:32 +0000

    add comment to R script

--- scriptR/Plot_all_trajectoir.R
+++ scriptR/Plot_all_trajectoir.R
@@ -1,45 +1,56 @@
-
+# Charger les bibliothèques nécessaires
 library(tidyverse)
 
-
+# Supprimer toutes les variables de l'environnement de travail
 rm(list = ls())
 
-## set working directory
+# Définir le répertoire de travail
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-
-# Lister les fichiers se terminant par 'traj-1.dat'
+# Lister les fichiers se terminant par 'traj-1.dat' dans le répertoire ../OUTPUT/
 fichiers <- list.files(path = "../OUTPUT/", pattern = "traj-1\\.dat$")
 
-
+# Initialiser des data frames vides pour stocker les données
 all.df <- data.frame()
 lastRow.df <- data.frame()
-for(i in 1:length(fichiers)){
-  df <- read.csv(paste0("../OUTPUT/",fichiers[i]),  header = FALSE, sep = "", stringsAsFactors = FALSE)
-  df$traj <- i 
+
+# Boucle pour lire chaque fichier et ajouter les données au data frame
+for (i in 1:length(fichiers)) {
+  # Lire les données du fichier courant
+  df <- read.csv(paste0("../OUTPUT/", fichiers[i]), header = FALSE, sep = "", stringsAsFactors = FALSE)
+  # Ajouter une colonne pour identifier la trajectoire
+  df$traj <- i
+  # Combiner les données du fichier courant avec le data frame principal
   all.df <- rbind(all.df, df)
+  # Ajouter la dernière ligne du fichier courant au data frame des dernières lignes
   lastRow.df <- rbind(lastRow.df, tail(df, n = 1))
 }
 
-write.csv(all.df, "/tmp/viab_all_traj.csv", row.names = F)
-write.csv(all.df, "/tmp/viab_all_traj_lastRow.csv", row.names = F)
-
-
+# Écrire les données combinées dans des fichiers CSV
+write.csv(all.df, "/tmp/viab_all_traj.csv", row.names = FALSE)
+write.csv(lastRow.df, "/tmp/viab_all_traj_lastRow.csv", row.names = FALSE)
 
 # Compter les occurrences des couples (V1, V2)
 counts <- all.df %>%
   group_by(V1, V2) %>%
-  summarise(count = n(), .groups = "drop")%>%
+  summarise(count = n(), .groups = "drop") %>%
   as.data.frame()
 
-colnames(counts) <- c("biomass", "capital", "occurence")
+# Renommer les colonnes du data frame counts
+colnames(counts) <- c("biomass", "capital", "occurrence")
 
-
-
+# Créer la heatmap hexagonale avec ggplot2
 ggplot() +
-  geom_hex(data = counts, aes(biomass, capital), bins = 50) +
-  geom_point(data = lastRow.df, aes(x = V1, y = V2), alpha = 0.2)+
+  # Ajouter les hexagones représentant la densité de points
+  geom_hex(data = counts, aes(x = biomass, y = capital, fill = occurrence), bins = 50) +
+  # Ajouter les points représentant les dernières lignes des trajectoires
+  geom_point(data = lastRow.df, aes(x = V1, y = V2), alpha = 0.2) +
+  # Définir le gradient de couleur pour la densité
   scale_fill_gradient(low = "#fee0d2", high = "#de2d26") +
-  labs(title = "Densité de point de toutes les trajectoires viables", fill = "Nombre\nde points\npar unité\nde surface",
+  # Ajouter les labels et le titre
+  labs(title = "Densité de point de toutes les trajectoires viables", 
+       fill = "Nombre\nde points\npar unité\nde surface",
        x = "Biomasse", y = "Capital") +
+  # Appliquer un thème minimaliste
   theme_bw()
+
